a94d1b4949d86616318b5a2e5b0243093a504741
Jan Pochyla <jpochyla@gmail.com>
2019-05-15 08:38:55 +0200

    core/tests: disable fading, improve runners

--- core/tests/run_tests_device_emu_monero.sh
+++ core/tests/run_tests_device_emu_monero.sh
@@ -7,13 +7,16 @@
 SDIR="$(SHELL_SESSION_FILE='' && cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 CORE_DIR="$SDIR/.."
 MICROPYTHON="$CORE_DIR/build/unix/micropython"
+DISABLE_FADE=1
 PYOPT=0
 upy_pid=""
 
 # run emulator if RUN_TEST_EMU
 if [[ $RUN_TEST_EMU > 0 ]]; then
   cd "$CORE_DIR/src"
-  "$MICROPYTHON" -O$PYOPT main.py >/dev/null &
+  TREZOR_TEST=1 \
+  TREZOR_DISABLE_FADE=$DISABLE_FADE \
+    "$MICROPYTHON" -O$PYOPT main.py >/dev/null &
   upy_pid=$!
   cd -
   sleep 1
@@ -26,7 +29,7 @@ DOCKER_ID=""
 terminate_test() {
   if [[ $# > 0 ]]; then error=$1; fi
   if [ -n "$upy_pid" ]; then kill $upy_pid 2> /dev/null; fi
-  if [ -n "$DOCKER_ID" ]; then docker kill $DOCKER_ID 2>/dev/null >/dev/null; fi 
+  if [ -n "$DOCKER_ID" ]; then docker kill $DOCKER_ID 2>/dev/null >/dev/null; fi
   exit $error
 }
 
@@ -58,7 +61,7 @@ error=1
 : "${TREZOR_MONERO_TESTS_PATH:=$CORE_DIR/tests/trezor_monero_tests}"
 : "${TREZOR_MONERO_TESTS_LOG:=$CORE_DIR/tests/trezor_monero_tests.log}"
 
-if [[ ! -f "$TREZOR_MONERO_TESTS_PATH" || "`shasum -a256 "$TREZOR_MONERO_TESTS_PATH" | cut -d' ' -f1`" != $TREZOR_MONERO_TESTS_SHA256SUM ]]; then 
+if [[ ! -f "$TREZOR_MONERO_TESTS_PATH" || "`shasum -a256 "$TREZOR_MONERO_TESTS_PATH" | cut -d' ' -f1`" != $TREZOR_MONERO_TESTS_SHA256SUM ]]; then
   echo "Downloading Trezor monero tests binary to `pwd`${TREZOR_MONERO_TESTS_PATH:1}"
   curl -# -L -o "$TREZOR_MONERO_TESTS_PATH" "$TREZOR_MONERO_TESTS_URL" \
     && chmod +x "$TREZOR_MONERO_TESTS_PATH" \
@@ -72,15 +75,15 @@ if [[ "$OSTYPE" == "linux-gnu" && "$FORCE_DOCKER_USE" != 1 ]]; then
   error=$?
 
 elif [[ "$OSTYPE" == "darwin"* || "$FORCE_DOCKER_USE" == 1 ]]; then
-  DOCKER_ID=$(docker run -idt --mount type=bind,src="$CORE_DIR",dst="$CORE_DIR" -w "$CORE_DIR" --network=host ubuntu:18.04)    
+  DOCKER_ID=$(docker run -idt --mount type=bind,src="$CORE_DIR",dst="$CORE_DIR" -w "$CORE_DIR" --network=host ubuntu:18.04)
   docker exec $DOCKER_ID apt-get update -qq 2>/dev/null >/dev/null
   docker exec $DOCKER_ID apt-get install --no-install-recommends --no-upgrade -qq net-tools socat 2>/dev/null >/dev/null
   docker exec -d $DOCKER_ID socat UDP-LISTEN:21324,reuseaddr,reuseport,fork UDP4-SENDTO:host.docker.internal:21324
   docker exec -d $DOCKER_ID socat UDP-LISTEN:21325,reuseaddr,reuseport,fork UDP4-SENDTO:host.docker.internal:21325
   docker exec $DOCKER_ID "$TREZOR_MONERO_TESTS_PATH" 2>&1 > "$TREZOR_MONERO_TESTS_LOG"
-  error=$? 
- 
-else 
+  error=$?
+
+else
   echo "Unsupported OS: $OSTYPE"
   exit 1
 fi
